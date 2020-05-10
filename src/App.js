3cf996c27e1968c2320a2fe2113d75a434c46298
Obinna Kelvin <okereobinna10@gmail.com>
2020-05-10 19:05:26 +0000

    New Logo and Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Hero from './components/Hero';
+import HowItWorks from './components/HowItWorks';
+import Subscribe from './components/Subscribe';
+import OurPromise from './components/OurPromise';
+import DownloadApp from './components/DownloadApp';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Hero />
+      <DownloadApp />
+      <HowItWorks />
+      <OurPromise />
+      <Subscribe/>
+      <Footer />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +30,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
